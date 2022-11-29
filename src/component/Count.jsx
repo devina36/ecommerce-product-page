f0519e9e6a9f606ff8822fd0936533e2b0700738
Devina Amalia <devina.amalia36@gmail.com>
2022-11-29 11:18:43 +0700

    button

--- src/component/Count.jsx
+++ src/component/Count.jsx
@@ -21,17 +21,21 @@ const Count = () => {
     <>
       <div className="flex flex-col md:flex-row gap-4 w-full">
         <div className="relative w-full md:max-w-[156px] flex justify-center items-center bg-light-grayish-blue rounded-xl">
-          <div
+          <button
             className="absolute px-5 md:px-3 h-full top-0 left-0 rounded-l-lg disabled:hover:opacity-100 hover:opacity-60"
             disabled={count === 0 || (count < 0 && true)}
             onClick={minus}
           >
-            <FaMinus size={16} className="text-oranges" />
-          </div>
+            <span>
+              <FaMinus size={16} className="text-oranges" />
+            </span>
+          </button>
           <span className="font-bold py-4">{count}</span>
-          <div className="absolute px-5 md:px-3 h-full top-0 right-0 rounded-r-lg hover:opacity-60" onClick={plus}>
-            <FaPlus size={16} className="text-oranges" />
-          </div>
+          <button className="absolute px-5 md:px-3 h-full top-0 right-0 rounded-r-lg hover:opacity-60" onClick={plus}>
+            <span>
+              <FaPlus size={16} className="text-oranges" />
+            </span>
+          </button>
         </div>
         <button
           onClick={addChart}
